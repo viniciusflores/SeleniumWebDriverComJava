efacaf0365deeab9aadf6bb2095b4eb6f9cba75e
Vinicius Flores <viniciusflores379@gmail.com>
2019-01-20 18:12:44 -0200

    Quit Browser

--- CursoSeleniumWebDriverComJava/src/main/java/TesteGoogle.java
+++ CursoSeleniumWebDriverComJava/src/main/java/TesteGoogle.java
@@ -1,20 +1,22 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.chrome.ChromeDriver;
 // import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+// import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteGoogle {
 	
 	@Test
 	public void teste() {
-		
-		WebDriver driver = new FirefoxDriver();
-		//WebDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
+		//WebDriver driver = new FirefoxDriver();
 		//WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
 		driver.get("http://www.google.com");
 		Assert.assertEquals("Google", driver.getTitle());
 		
+		
+		driver.quit();
 	}	
 }
