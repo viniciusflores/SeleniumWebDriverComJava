7f045eaaa809b25d2fb0383e8ee33a77c18378ac
Vinicius Flores <viniciusflores379@gmail.com>
2019-01-20 17:57:26 -0200

    Demais browsers

--- CursoSeleniumWebDriverComJava/src/main/java/TesteGoogle.java
+++ CursoSeleniumWebDriverComJava/src/main/java/TesteGoogle.java
@@ -1,6 +1,8 @@
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteGoogle {
@@ -9,6 +11,8 @@ public class TesteGoogle {
 	public void teste() {
 		
 		WebDriver driver = new FirefoxDriver();
+		//WebDriver driver = new ChromeDriver();
+		//WebDriver driver = new EdgeDriver();
 		driver.get("http://www.google.com");
 		Assert.assertEquals("Google", driver.getTitle());
 		
