7778668083324728d0467f996a7b8bc6a1b57a29
Vinicius Flores <viniciusflores379@gmail.com>
2019-01-20 17:43:49 -0200

    First open browser with selenium

--- CursoSeleniumWebDriverComJava/src/main/java/TesteGoogle.java
+++ CursoSeleniumWebDriverComJava/src/main/java/TesteGoogle.java
@@ -1,9 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteGoogle {
-
 	public static void main(String[] args) {
-		System.out.println("Hello Teste");
-
-	}
-
+		// System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.google.com");
+		System.out.println(driver.getTitle());
+		
+	}	
 }
