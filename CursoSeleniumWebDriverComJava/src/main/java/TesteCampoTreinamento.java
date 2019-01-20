cf3fdbecbe3f770ea86a93802115973502c63d88
Vinicius Flores <viniciusflores379@gmail.com>
2019-01-20 19:18:56 -0200

    Select e Radio

--- CursoSeleniumWebDriverComJava/src/main/java/TesteCampoTreinamento.java
+++ CursoSeleniumWebDriverComJava/src/main/java/TesteCampoTreinamento.java
@@ -27,10 +27,32 @@ public class TesteCampoTreinamento {
 		
 		WebElement txtSugestoes = driver.findElement(By.id("elementosForm:sugestoes"));
 		txtSugestoes.sendKeys("Vili\ncius\nFlores");
-		Assert.assertEquals("Vili\\ncius\\nFlores", txtSugestoes.getAttribute("value"));
+		Assert.assertEquals("Vili\ncius\nFlores", txtSugestoes.getAttribute("value"));
 		driver.quit();
 	}
 	
+	@Test
+	public void testeRadioButton() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		WebElement radBtn = driver.findElement(By.id("elementosForm:sexo:0"));
+		radBtn.click();
+		Assert.assertTrue(radBtn.isSelected());
+		driver.quit();
+	}
 	
+	@Test
+	public void testeCheckBox() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		WebElement checkPizza = driver.findElement(By.id("elementosForm:comidaFavorita:2"));
+		checkPizza.click();
+		Assert.assertTrue(checkPizza.isSelected());
+		driver.quit();
+	}
 	
 }
