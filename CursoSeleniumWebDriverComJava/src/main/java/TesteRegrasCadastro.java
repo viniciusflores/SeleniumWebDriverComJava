95b856cddc022107dcee7963ce12fd939faf3ca3
Vinicius Flores <viniciusflores379@gmail.com>
2019-01-27 18:04:01 -0200

    Refactory

--- CursoSeleniumWebDriverComJava/src/main/java/TesteRegrasCadastro.java
+++ CursoSeleniumWebDriverComJava/src/main/java/TesteRegrasCadastro.java
@@ -18,20 +18,20 @@ public class TesteRegrasCadastro {
 	private WebDriver driver;
 	private DSL dsl;
 	private CampoTreinamentoPage page;
-	
+
 	@Parameter
 	public String nome;
-	@Parameter(value=1)
+	@Parameter(value = 1)
 	public String sobrenome;
-	@Parameter(value=2)
+	@Parameter(value = 2)
 	public String sexo;
-	@Parameter(value=3)
+	@Parameter(value = 3)
 	public List<String> comidas;
-	@Parameter(value=4)
+	@Parameter(value = 4)
 	public String[] esportes;
-	@Parameter(value=5)
+	@Parameter(value = 5)
 	public String msg;
-	
+
 	@Before
 	public void inicializa() {
 		driver = new ChromeDriver();
@@ -40,38 +40,42 @@ public class TesteRegrasCadastro {
 		dsl = new DSL(driver);
 		page = new CampoTreinamentoPage(driver);
 	}
-	
+
 	@After
 	public void finaliza() {
 		driver.quit();
 	}
-	
+
 	@Parameters
 	public static Collection<Object[]> getCollection() {
-		return Arrays.asList(new Object[][] {
-			{"", "", "", Arrays.asList(), new String[] {}, "Nome eh obrigatorio"},
-			{"Wagner", "", "", Arrays.asList(), new String[]{}, "Sobrenome eh obrigatorio"},
-			{"Wagner", "Costa", "", Arrays.asList(), new String[]{}, "Sexo eh obrigatorio"},
-			{"Wagner", "Costa", "Masculino", Arrays.asList("Carne", "Vegetariano"), new String[]{}, "Tem certeza que voce eh vegetariano?"},
-			{"Wagner", "Costa", "Masculino", Arrays.asList("Carne"), new String[]{"Karate", "O que eh esporte?"}, "Voce faz esporte ou nao?"}
-		});
+		return Arrays.asList(new Object[][] { { "", "", "", Arrays.asList(), new String[] {}, "Nome eh obrigatorio" },
+				{ "Wagner", "", "", Arrays.asList(), new String[] {}, "Sobrenome eh obrigatorio" },
+				{ "Wagner", "Costa", "", Arrays.asList(), new String[] {}, "Sexo eh obrigatorio" },
+				{ "Wagner", "Costa", "Masculino", Arrays.asList("Carne", "Vegetariano"), new String[] {},
+						"Tem certeza que voce eh vegetariano?" },
+				{ "Wagner", "Costa", "Masculino", Arrays.asList("Carne"),
+						new String[] { "Karate", "O que eh esporte?" }, "Voce faz esporte ou nao?" } });
 	}
-	
+
 	@Test
 	public void deveValidarRegras() {
 		page.setNome(nome);
 		page.setSobrenome(sobrenome);
-		if(sexo.equals("Masculino")) {
+		if (sexo.equals("Masculino")) {
 			page.setSexoMasculino();
 		}
-		else {
+		if (sexo.equals("Feminino")) {
 			page.setSexoFeminino();
 		}
-		if(comidas.contains("Carne")) page.setComidaFavCarne();
-		if(comidas.contains("Frango")) page.setComidaFavFrango();
-		if(comidas.contains("Pizza")) page.setComidaFavPizza();
-		if(comidas.contains("Vegetariano")) page.setComidaFavVegetariano();
-		
+		if (comidas.contains("Carne"))
+			page.setComidaFavCarne();
+		if (comidas.contains("Frango"))
+			page.setComidaFavFrango();
+		if (comidas.contains("Pizza"))
+			page.setComidaFavPizza();
+		if (comidas.contains("Vegetariano"))
+			page.setComidaFavVegetariano();
+
 		page.setEsportes(esportes);
 		page.cadastrar();
 		Assert.assertEquals(msg, dsl.alertaObterTextoEAceita());
