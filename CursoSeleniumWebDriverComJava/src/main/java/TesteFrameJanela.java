29a1570ee76b8d32d4565070296f0a8d95274eb6
Vinicius Flores <viniciusflores379@gmail.com>
2019-01-27 19:49:17 -0200

    JS 2

--- CursoSeleniumWebDriverComJava/src/main/java/TesteFrameJanela.java
+++ CursoSeleniumWebDriverComJava/src/main/java/TesteFrameJanela.java
@@ -4,7 +4,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
@@ -16,14 +18,15 @@ public class TesteFrameJanela {
 	@Before
 	public void inicializa() {
 		driver = new ChromeDriver();
-		driver.manage().window().maximize();
+		//driver.manage().window().maximize();
+		driver.manage().window().setSize(new Dimension(1200, 765));
 		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
 		dsl = new DSL(driver);
 	}
 
 	@After
 	public void finaliza() {
-		driver.quit();
+		//driver.quit();
 	}
 	
 	@Test
@@ -35,6 +38,16 @@ public class TesteFrameJanela {
 
 		dsl.sairFrame();
 		dsl.escrever("elementosForm:nome", msg);
+	}
+	
+	@Test
+	public void deveInteragirComFrameEscondido(){
+		WebElement frame = driver.findElement(By.id("frame2"));
+		dsl.executarJS("window.scrollBy(0, arguments[0])", frame.getLocation().y);
+		dsl.entrarFrame("frame2");
+		dsl.clicarBotao("frameButton");
+		String msg = dsl.alertaObterTextoEAceita();
+		Assert.assertEquals("Frame OK!", msg);
 	}
 	
 	@Test
