4525fe47b3f85bec55f7b53223e9fefa5b9a5bc2
Vinicius Flores <viniciusflores379@gmail.com>
2019-02-02 14:14:13 -0200

    Espera fixa

---
+++ CursoSeleniumWebDriverComJava/src/main/java/TesteSincronismo.java
@@ -0,0 +1,33 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteSincronismo {
+	private WebDriver driver;
+	private DSL dsl;
+
+	@Before
+	public void inicializa() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		dsl = new DSL(driver);
+	}
+
+	@After
+	public void finaliza() {
+		driver.quit();
+	}
+	
+	@Test
+	public void clicarBotaoComRespostaDemorada() throws InterruptedException {
+		dsl.clicarBotao("buttonDelay");
+		Thread.sleep(5000);
+		dsl.escrever("novoCampo", "Deu certo?");
+		Assert.assertEquals("Deu certo?", dsl.obterTexto("novoCampo"));
+		
+	}
+}
