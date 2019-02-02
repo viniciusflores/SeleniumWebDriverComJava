ea2fc62e0804545bb66847a5200c8e57a9c50c1c
Vinicius Flores <viniciusflores379@gmail.com>
2019-02-02 18:24:59 -0200

    Screenshot

--- CursoSeleniumWebDriverComJava/src/main/java/core/BaseTest.java
+++ CursoSeleniumWebDriverComJava/src/main/java/core/BaseTest.java
@@ -1,13 +1,30 @@
 package core;
 
+import static core.DriverFactory.getDriver;
 import static core.DriverFactory.killDriver;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class BaseTest {
 
+	@Rule
+	public TestName testNome = new TestName();
+	
 	@After
-	public void finaliza(){
+	public void finaliza() throws IOException{
+		TakesScreenshot ss = (TakesScreenshot) getDriver();
+		File arquivo = ss.getScreenshotAs(OutputType.FILE); 
+		FileUtils.copyFile(arquivo, new File("target" + File.separator + "screenshot"  
+				+ File.separator + testNome.getMethodName() + ".jpg"));
+		
 		if(Propiedades.FECHAR_BROWSER) {
 			killDriver();
 		}
