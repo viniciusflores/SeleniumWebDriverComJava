bc441573456bf39d0a02b04628b8033376de9e6b
Vinicius Flores <viniciusflores379@gmail.com>
2019-02-02 18:35:21 -0200

    Browsers

--- CursoSeleniumWebDriverComJava/src/main/java/core/DriverFactory.java
+++ CursoSeleniumWebDriverComJava/src/main/java/core/DriverFactory.java
@@ -2,21 +2,28 @@ package core;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
 	private static WebDriver driver;
 	
-	private DriverFactory() {
-		
-	}
+	private DriverFactory() { }
 	
 	public static WebDriver getDriver() {
 		if (driver == null) {
-			driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		return driver;
+			switch (Propiedades.browser) {
+				case FIREFOX: driver = new FirefoxDriver();
+				break;
+				case CHROME: driver = new ChromeDriver();
+					break;
+				default:
+					driver = new ChromeDriver();
+					break;
+				}
+			driver.manage().window().maximize();
 		}
 		return driver;
+
 	}
 	
 	public static void killDriver() {
--- CursoSeleniumWebDriverComJava/src/main/java/core/Propiedades.java
+++ CursoSeleniumWebDriverComJava/src/main/java/core/Propiedades.java
@@ -3,4 +3,11 @@ package core;
 public class Propiedades {
 	// manter false para fechar o navegador a cada test
 	public static boolean FECHAR_BROWSER = true;
+	
+	public static Browsers browser = Browsers.CHROME;
+	
+	public enum Browsers {
+		CHROME,
+		FIREFOX
+	}
 }
