9d6cdb3c25528ce10c3f50e62b0e2f98343dc4a8
Vinicius Flores <viniciusflores379@gmail.com>
2019-02-02 14:31:47 -0200

    Esperas

--- CursoSeleniumWebDriverComJava/src/main/java/TesteSincronismo.java
+++ CursoSeleniumWebDriverComJava/src/main/java/TesteSincronismo.java
@@ -1,9 +1,14 @@
+import java.util.concurrent.TimeUnit;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TesteSincronismo {
 	private WebDriver driver;
@@ -23,11 +28,29 @@ public class TesteSincronismo {
 	}
 	
 	@Test
-	public void clicarBotaoComRespostaDemorada() throws InterruptedException {
+	public void deveUtilizarEsperaFixa() throws InterruptedException {
 		dsl.clicarBotao("buttonDelay");
-		Thread.sleep(5000);
+		Thread.sleep(10000);
 		dsl.escrever("novoCampo", "Deu certo?");
 		Assert.assertEquals("Deu certo?", dsl.obterTexto("novoCampo"));
-		
 	}
+	
+	@Test
+	public void deveUtilizarEsperaImplicita() throws InterruptedException {
+		dsl.clicarBotao("buttonDelay");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		dsl.escrever("novoCampo", "Deu certo?");
+		Assert.assertEquals("Deu certo?", dsl.obterTexto("novoCampo"));
+	}
+	
+	@Test
+	public void deveUtilizarEsperaExplicita() throws InterruptedException {
+		dsl.clicarBotao("buttonDelay");
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("novoCampo")));
+		dsl.escrever("novoCampo", "Deu certo?");
+		Assert.assertEquals("Deu certo?", dsl.obterTexto("novoCampo"));
+	}
+
+
 }
